9227ab1d1e1f6d6468754f62621386adf93c3a94
Dan (wait for it) Napierkowski <Dan.Napierkowski@gmail.com>
2020-01-09 19:24:48 -0600

    WIP
    Pushed some more code to connect to wifi, working on setting static IP

--- WirelessEstop/WirelessEstop.ino
+++ WirelessEstop/WirelessEstop.ino
@@ -1,9 +1,31 @@
+#include <BearSSLHelpers.h>
+#include <CertStoreBearSSL.h>
 #include <ESP8266WiFi.h>
-#include "RoveComm.h"
+#include <ESP8266WiFiAP.h>
+#include <ESP8266WiFiGeneric.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266WiFiScan.h>
+#include <ESP8266WiFiSTA.h>
+#include <ESP8266WiFiType.h>
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+#include <WiFiClientSecureAxTLS.h>
+#include <WiFiClientSecureBearSSL.h>
+#include <WiFiServer.h>
+#include <WiFiServerSecure.h>
+#include <WiFiServerSecureAxTLS.h>
+#include <WiFiServerSecureBearSSL.h>
+#include <WiFiUdp.h>
+
+#include <RoveComm.h>
+#include <RoveCommManifest.h>
+#include <RoveCommPacket.h>
+
+#include <ESP8266WiFi.h>
 
 #ifndef STASSID
-#define STASSID "<ID>"
-#define STAPSK  "<PSSWD>"
+#define STASSID "ScrewYouEli"
+#define STAPSK  "69696969"
 #endif
 
 RoveCommWifiUdp RoveComm;
@@ -11,17 +33,15 @@ RoveCommWifiUdp RoveComm;
 void setup() 
 {
   Serial.begin(115200);
-  WiFi.mode(WIFI_STA);
+  //WiFi.mode(WIFI_STA);
+  IPAddress ip(192,168,137,45);   
+  WiFi.config(ip);
   WiFi.begin(STASSID, STAPSK);
   while (WiFi.status() != WL_CONNECTED) 
   {
     Serial.print('.');
     delay(500);
   }
-  IPAddress ip(192,168,1,200);   
-  IPAddress gateway(192,168,1,254);   
-  IPAddress subnet(255,255,255,0);   
-  WiFi.config(ip, gateway, subnet);
 
   Serial.print("Connected! IP address: ");
   Serial.println(WiFi.localIP());
@@ -30,10 +50,10 @@ void setup()
 void loop() 
 {
   //read in any incoming packets, and display the data ids
-  rovecomm_packet packet;
-  packet = RoveComm.read();
-  Serial.println(packet.data_id);
+  //rovecomm_packet packet;
+  //packet = RoveComm.read();
+  //Serial.println(packet.data_id);
+  Serial.println("Boo");
 
-
-  RoveComm.writeTo(RC_BMSBOARD_SWESTOPs_DATAID, 1, 10, 192, 168, 1, RC_BMSBOARD_FOURTHOCTET, 11000)
+  //RoveComm.writeTo(RC_BMSBOARD_SWESTOPs_DATAID, 1, 10, 192, 168, 137, 1, 11000);
 }
